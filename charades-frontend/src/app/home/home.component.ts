53ab6e9602763c984667572e366ac0f5aaa758ea
Andrzej Lech <andrzejdlech@gmail.com>
2020-12-06 11:53:16 +0100

    change sass to scss and add themes

--- charades-frontend/src/app/home/home.component.ts
+++ charades-frontend/src/app/home/home.component.ts
@@ -1,24 +1,45 @@
 import {Component, OnInit} from '@angular/core';
 import {AuthenticationService} from '../_services/authentication.service';
-import { MatDialog } from '@angular/material/dialog';
-import { SignInComponent } from '../sign-in/sign-in.component';
-import { SignUpComponent } from '../sign-up/sign-up.component';
+import {MatDialog} from '@angular/material/dialog';
+import {SignInComponent} from '../sign-in/sign-in.component';
+import {SignUpComponent} from '../sign-up/sign-up.component';
+import {ColorSchemeService} from '../_services/color-scheme.service';
 
 @Component({
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.sass']
 })
-export class HomeComponent implements OnInit{
+export class HomeComponent implements OnInit {
 
-  constructor(private authenticationService: AuthenticationService, public dialog: MatDialog) {
+
+  constructor(private authenticationService: AuthenticationService,
+              public dialog: MatDialog,
+              private colorSchemeService: ColorSchemeService) {
+    this.colorSchemeService.load();
   }
 
+
+  themes = [
+    {
+      name: 'dark',
+      icon: 'brightness_3'
+    },
+    {
+      name: 'light',
+      icon: 'wb_sunny'
+    }
+  ];
+
   openSignIn(): void {
-    this.dialog.open(SignInComponent, {})
+    this.dialog.open(SignInComponent, {});
   }
 
   openSignUp(): void {
-    this.dialog.open(SignUpComponent, {})
+    this.dialog.open(SignUpComponent, {});
+  }
+
+  setTheme(theme: string): void {
+    this.colorSchemeService.update(theme);
   }
 
   ngOnInit(): void {
