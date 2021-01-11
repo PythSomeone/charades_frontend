720015cd2bd30ef76ad388eb530ddff77fb73ce4
Andrzej Lech <andrzejdlech@gmail.com>
2021-01-11 19:15:28 +0100

    Update error.interceptor.ts

--- charades-frontend/src/app/_helpers/error.interceptor.ts
+++ charades-frontend/src/app/_helpers/error.interceptor.ts
@@ -2,7 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
-import {AuthenticationService} from '@app/_services/authentication.service';
+import {AuthenticationService} from '../_services/authentication.service';
+
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
@@ -12,7 +13,7 @@ export class ErrorInterceptor implements HttpInterceptor {
     return next.handle(request).pipe(catchError(err => {
       if (err.status === 401) {
         // auto logout if 401 response returned from api
-        this.authenticationService.logout();
+        this.authenticationService.logOut();
         location.reload(true);
       }
 
